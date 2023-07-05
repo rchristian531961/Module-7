c013ee816828f6a3e3e12a15abe573efa15a23a6
Ray Christian <rchristian531961@yahoo.com>
2023-07-05 13:49:52 -0500

    Working on backend and designing

--- Lab-3/src/Components/Postform.jsx
+++ Lab-3/src/Components/Postform.jsx
@@ -1,19 +1,19 @@
 export default function PostForm(props) {
-    var {title,myName}=props;
+    var {handleSubmit,handleText,handleTitle, handleName,title,myName,myText}=props;
     return(
         <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Title:
                 {/* <input type='text' onChange={(e)=>{setCatName(e.target.value)}} name='catName' value={catName}></input> */}
-                <input type='text' onChange={()=>{}} name='title' value={title}></input>
+                <input type='text' onChange={(e)=>{handleTitle(e.target.value)}} name='title' value={title}></input>
             </label><br/>
             <label>Name: 
                 {/* <input type='text' onChange={(e)=>{setNewLatinName(e.target.value)}} name='latinName' value={newLatinName}></input> */}
-              <input type='text' onChange={()=>{}} name='myName' value={myName}></input>
+              <input type='text' onChange={(e)=>{handleName(e.target.value)}} name='myName' value={myName}></input>
             </label><br/>
             <label>
                 Edit your Post:<br/>
-                <textarea name='text' rows={4} cols={40} defaultValue="Make your Post Here"/>
+                <textarea onChange={(e)=>{handleText(e.target.value)}} name='text' rows={4} cols={40} defaultValue="Make your Post Here"/>
             </label>
             <br/>
             <button>Submit</button>
